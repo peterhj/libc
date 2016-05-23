0c6683f93cf87849d2b812273a66d8016037c11a
Peter Jin <peterhaijin@gmail.com>
2016-05-23 07:49:54 -0700

    Give `cpu_set_t` field `bits` public visibility so it can be initialized.

--- src/unix/notbsd/linux/mod.rs
+++ src/unix/notbsd/linux/mod.rs
@@ -177,9 +177,9 @@ s! {
 
     pub struct cpu_set_t {
         #[cfg(target_pointer_width = "32")]
-        bits: [u32; 32],
+        pub bits: [u32; 32],
         #[cfg(target_pointer_width = "64")]
-        bits: [u64; 16],
+        pub bits: [u64; 16],
     }
 }
 
